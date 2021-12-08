b61c68f6982d8c4c6ea6605612a386dafbcd78b9
konatsu.hori <konatsu.hori@sportsbull.jp>
2021-12-08 18:41:08 +0900

    initial

---
+++ web/index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<link href="index.keiziban.css" rel="stylesheet" type="text/css" media="all">
+<title>掲示板</title>
+</head>
+<body>
+<h1>掲示板</h1>
+<section>
+    <h2>新規投稿</h2>
+    <form action="" method="post">
+        <div class="name"><span class="label">名前: <input type="text" name="name" value=""><br>
+        <div class="honbun"><span class="label">本文: <textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard"
+        placeholder="100字以内で入力してください。"></textarea>
+        < type="submit">投稿</button>
+    </form>
+</section>
+<section class=""toukou>
+    <h2>投稿一覧</h2>
+    <p>投稿はまだありません</p>
+</section>
+</body>
+</html>
