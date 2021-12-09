643e7a73d9b028c1af9a52a612dfc44898c51c63
konatsu.hori <konatsu.hori@sportsbull.jp>
2021-12-09 18:40:30 +0900

    PHP組み込み

--- web/index.php
+++ web/index.php
@@ -1,3 +1,27 @@
+<?php
+
+//メッセージを保存するファイルパス設定
+define( 'FILENAME', './message.txt');
+
+//タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+if( !empty($_POST['btn_submit']) ) {
+　　　var_dump($_POST);
+
+     if ( $file_handle = fopen( FILENAME, "a") ) {
+
+        // 書き込み日時を取得
+        $current_date = date("Y-m-d H:i:s");
+
+        //書き込み
+        fwrite( $file_handle, $data);
+
+        //ファイルを閉じる
+        fclose( $file_handle);
+     }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -14,10 +38,11 @@
         <div class="name"><span class="label">名前: <input type="text" name="name" value=""><br>
         <div class="honbun"><span class="label">本文: <textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard"
         placeholder="100字以内で入力してください。"></textarea>
-        <button type="submit">投稿</button>
+       <br> <button type="submit">投稿</button>
     </form>
 </section>
 <section class=""toukou>
+    <hr width="60%">
     <h2>投稿一覧</h2>
     <p>投稿はまだありません</p>
 </section>
