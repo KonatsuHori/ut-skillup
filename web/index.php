c1de0425842a97a90ef19a79557452c790ac433a
konatsu.hori <konatsu.hori@sportsbull.jp>
2021-12-16 18:59:36 +0900

    投稿内容、日時の表示

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 //メッセージを保存するファイルパス設定
 define( 'FILENAME', './message.txt');
@@ -6,13 +6,29 @@ define( 'FILENAME', './message.txt');
 //タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
 
+// 変数の初期化
+$current_date = null;
+$data = null;
+$file_handle = null;
+$split_data = null;
+$message = array();
+$message_array = array();
+
+//var_dump($_POST);
+//$file_handle = fopen( FILENAME, "a");
+//fwrite($file_handle, "hoge");
+//fclose( $file_handle);
+
+///var_dump($_POST);
 if( !empty($_POST['btn_submit']) ) {
-　　　var_dump($_POST);
 
      if ( $file_handle = fopen( FILENAME, "a") ) {
 
         // 書き込み日時を取得
         $current_date = date("Y-m-d H:i:s");
+
+        // 書き込むデータを作成
+        $data = "'".$_POST['name']."','".$_POST['comment']."','".$current_date."'\n";
 
         //書き込み
         fwrite( $file_handle, $data);
@@ -21,6 +37,25 @@ if( !empty($_POST['btn_submit']) ) {
         fclose( $file_handle);
      }
 }
+
+
+if( $file_handle = fopen( FILENAME,'r') ) {
+    while( $data = fgets($file_handle) ){
+        
+        $split_data = preg_split( '/\'/', $data);
+
+        $message = array(
+            'name' => $split_data[1],
+            'message' => $split_data[3],
+            'post_date' => $split_data[5]
+        );
+        array_unshift( $message_array, $message);
+    }
+
+    // ファイルを閉じる
+    fclose( $file_handle);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -38,13 +73,25 @@ if( !empty($_POST['btn_submit']) ) {
         <div class="name"><span class="label">名前: <input type="text" name="name" value=""><br>
         <div class="honbun"><span class="label">本文: <textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard"
         placeholder="100字以内で入力してください。"></textarea>
-       <br> <button type="submit">投稿</button>
+       <br> <input type="submit" name="btn_submit" value="投稿">
     </form>
 </section>
-<section class=""toukou>
+<section>
     <hr width="60%">
     <h2>投稿一覧</h2>
-    <p>投稿はまだありません</p>
+
+<?php if( !empty($message_array) ): ?>
+<?php foreach( $message_array as $value ): ?>
+<article>
+    <div class="info">
+        <p><?php echo $value['name']; ?></p>
+        <time><?php echo date('Y年m月d日 H:i:s', strtotime($value['post_date'])); ?>
+</time>
+    </div>
+    <p><?php echo $value['message']; ?></p>
+</article>
+<?php endforeach; ?>
+<?php endif; ?>
 </section>
 </body>
 </html>
